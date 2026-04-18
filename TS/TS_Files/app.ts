432ae854645718cdf8d81b29e9414b3a41f47ee2
suraj <surajade1960192@gmail.com>
2026-04-18 21:56:10 +0530

    Updated few files

--- TS/TS_Files/app.ts
+++ TS/TS_Files/app.ts
@@ -1,4 +1,13 @@
+//TS --> define types with your variables, functions etc.
 
 let message: string = 'Hello TS';
+console.log(message);//Hello TS
 
-console.log(message);
+let x: number = 100;
+console.log(x);//100
+
+function testing(browser: string): string{
+    return browser.toLowerCase();
+}
+let result: string = testing('CHROME');
+console.log(result);//chrome
